4c19b2c9dd212a99dcc16d2048a57db2bcc78df7
mobinpda-lab <mobinpda@gmail.com>
2026-09-21 19:53:32 +0330

    chore(migration): move Joplin editor files from Arvin-clean

---
+++ research/legacy-persian-shamsi-plugin/plugin/src/editorFont.ts
@@ -0,0 +1,4 @@
+export const EDITOR_FONT_SETTING = 'editorFontFamily';
+export const EDITOR_FONT_COMMAND = 'joplin-persian-shamsi-set-editor-font';
+export const VAZIRHARF_FONT = '"VazirHarf", Tahoma, sans-serif';
+export const DEFAULT_EDITOR_FONT = 'default';
